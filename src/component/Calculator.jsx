bf414159eb62b3adcbd1327ca72f9e29d8c48bde
Tunisia Artope <artope@yahoo.com>
2022-06-17 12:14:17 -0400

    initial commit

---
+++ src/component/Calculator.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Calculator(){
+
+    return(
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <h1 className="display-5 fw-bolder text-center text-primary">CALCULATOR</h1>
+                        <hr />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Calculator;
